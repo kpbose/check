ed7062e08f1ef219c5cc5f5ec377d51b8c60db06
Koushal_1 <75929290+kpbose@users.noreply.github.com>
2022-07-02 16:59:10 +0530

    Add files via upload

---
+++ tkinter2.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root=Tk()
+root.geometry('300x300')
+name=Label(text='!!!!!!!ERROR!!!!!!!!!')
+name1=Label(text='YE TERE BAS KI BAAT NHI')
+name.pack()
+name1.pack()
+root.mainloop()
